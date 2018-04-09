813180e6fb1fa728fc61532376b6c2cceab0beb5
DavidNganga <ngashiedavid@gmail.com>
2018-04-09 12:47:21 +0300

    added bootstrap

--- app/views.py
+++ app/views.py
@@ -10,12 +10,15 @@ def index():
     '''
     category_news = get_sources('source')
     print(category_news)
+
+
+
     title = 'Home'
     message = 'On the news today!!'
-    return render_template('index.html',title = title, message = message)
+    return render_template('index.html',title = title, message = message, sources=category_news)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
 
    
-    return render_template('news.html',id = news_id)
+    return render_template('news.html',id = news_id, )
